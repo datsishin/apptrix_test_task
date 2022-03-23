f2b0ebb1969b2a6dc34b87ef6a39e0c5d54da646
Егор <gok6@inbox.ru>
2022-03-23 18:17:27 +0300

    v2.2.4.2
    1. Исправления для деплоя на Heroku v3

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -112,11 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -151,3 +146,14 @@ CACHES = {
 }
 
 AUTH_USER_MODEL = 'api.CustomUser'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '/Users/egor_datsishin/Dev/apptrix_test_task/staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
