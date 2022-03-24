e16afc22d3f0b54beb94ae994362d3f3a6a5e5b9
Егор <gok6@inbox.ru>
2022-03-24 16:58:02 +0300

    v2.2.7.13
    1. Небольшие правки

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'dating_app.urls'
@@ -160,3 +161,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
